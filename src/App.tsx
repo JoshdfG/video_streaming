20fa9351c703393bf2db0a937323c88eb46cd8d7
joshdfg <slimmy0310@gmail.com>
2024-09-03 17:33:58 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import {
+  Call,
+  CallControls,
+  CallingState,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+  useCallStateHooks,
+  User,
+} from "@stream-io/video-react-sdk";
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "./App.css";
+
+// For demo credentials, check out our video calling tutorial:
+// https://getstream.io/video/sdk/react/tutorial/video-calling/
+const apiKey = "mmhfdzb5evj2";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0FuYWtpbl9Tb2xvIiwidXNlcl9pZCI6IkFuYWtpbl9Tb2xvIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3MjUzOTc2MzEsImV4cCI6MTcyNjAwMjQzMX0.77tW0s2Hy3Kgmvy-l3W9nitp9csojPJHcSpJIWFhofY";
+const userId = "Anakin_Solo";
+const callId = "0b6Rb7l0HClP";
+
+const user: User = {
+  id: userId,
+  name: "Oliver",
+  image: "https://getstream.io/random_svg/?id=oliver&name=Oliver",
+};
+
+// initialize the StreamVideoClient
+const client = new StreamVideoClient({ apiKey, user, token });
+
+export default function App() {
+  const [call, setCall] = useState<Call>();
+  useEffect(() => {
+    const myCall = client.call("default", callId);
+    myCall.join({ create: true }).catch((err) => {
+      console.error(`Failed to join the call`, err);
+    });
+
+    setCall(myCall);
+
+    return () => {
+      setCall(undefined);
+      myCall.leave().catch((err) => {
+        console.error(`Failed to leave the call`, err);
+      });
+    };
+  }, []);
+
+  if (!call) return null;
+
+  return (
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <UILayout />
+      </StreamCall>
+    </StreamVideo>
+  );
+}
+
+export const UILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <StreamTheme>
+      <SpeakerLayout participantsBarPosition="bottom" />
+      <CallControls />
+    </StreamTheme>
+  );
+};
